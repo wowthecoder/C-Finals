820b13f124a531829bbbc46abfcef93c67a888ad
jhl323 <jhl323@ic.ac.uk>
2024-06-10 00:40:19 +0100

    Complete part A and q1-2 of part B

--- laser/laser.c
+++ laser/laser.c
@@ -4,35 +4,143 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 bool is_on_board(board_t board, int row, int col) {
-    return true;
+    return row >= 0 && row < board.height && col >= 0 && col < board.width;
 }
 
 int find_start(board_t board) {
+    for (int r = 0; r < board.height; r++) {
+        if (board.cells[r][0] == '>') {
+            return r;
+        }
+    }
     return -1;
 }
 
 char get_mirror_label(board_t board, int row, int col) {
     assert(is_on_board(board, row, col));
+    if (strchr(MIRRORS, board.cells[row][col]) == NULL) {
+        return '\0';
+    }
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            char ch = board.cells[row + i][col + j];
+            if (isalpha(ch)) {
+                return ch;
+            }
+        }
+    }
     return '\0';
 }
 
 board_t load_board(const char *filename) {
     board_t board;
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) {
+        fprintf(stderr, "Cannot open file, gimme another\n");
+        return board;
+    }
+    fscanf(f, "%d %d\n", &board.height, &board.width);
+    int h = board.height, w = board.width;
+    board.cells = malloc(h * sizeof(char *));
+    if (board.cells == NULL) {
+        fprintf(stderr, "Cannot alloc memory for board.cells ?!?!\n");
+        return board;
+    }
+    board.cells[0] = malloc(h * w * sizeof(char));
+    if (board.cells[0] == NULL) {
+        fprintf(stderr, "Cannot alloc memory for board.cells[0] ?!?!\n");
+        free ( board.cells );
+        return board;
+    }
+
+    // now wire up the row pointers correctly
+    for (int i = 1; i < h; i++) {
+        board.cells[i] = board.cells[i-1] + w;
+    }
+
+    // now read the board
+    char buffer[w + 2]; // read the 32 characters, plus \n and the NULL terminator
+    int i = 0;
+    while (fgets(buffer, sizeof(buffer), f) != NULL) {
+        for (int j = 0; j < w; j++) {
+            board.cells[i][j] = buffer[j];
+        }
+        // printf("i is %d, line : %s, strlen : %d\n", i, buffer, strlen(buffer));
+        i++;
+    }
+    fclose(f);
     return board;
 }
 
 void free_cells(board_t board) {
+    free ( board.cells[0] );
+    free ( board.cells );
 }
 
 direction_t change_direction(direction_t direction, char cell) {
+    if (cell == FWD_MIRROR) {
+        switch (direction) {
+            case EAST:
+                return NORTH;
+            case SOUTH:
+                return WEST;
+            case WEST: 
+                return SOUTH;
+            case NORTH:
+                return EAST;
+        }
+    } else if (cell == BWD_MIRROR)  {
+        switch (direction) {
+            case EAST:
+                return SOUTH;
+            case SOUTH:
+                return EAST;
+            case WEST: 
+                return NORTH;
+            case NORTH:
+                return WEST;
+        }
+    }
+    // not a mirror
     return direction;
+}
+
+void append(char* s, char c) {
+        int len = strlen(s);
+        s[len] = c;
+        s[len+1] = '\0';
 }
 
 char step_laser(board_t board, laser_state_t *laser) {
     assert(is_on_board(board, laser->row, laser->col));
-    return '\0';
+    // Change direction if current cell is a mirror
+    char cell = board.cells[laser->row][laser->col];
+    laser->direction = change_direction(laser->direction, cell);
+    // advance laser beam 1 step
+    switch (laser->direction) {
+        case EAST: // to right side
+            laser->col++;
+            break;
+        case SOUTH: // downwards
+            laser->row++;
+            break;
+        case WEST: // to left side
+            laser->col--;
+            break;
+        case NORTH: // upwards
+            laser->row--;
+            break;
+    }
+    // update laser message
+    char label = get_mirror_label(board, laser->row, laser->col);
+    if (label != '\0') {
+        append(laser->msg, label);
+    }
+    return board.cells[laser->row][laser->col];
 }
 
 bool shoot(board_t board, laser_state_t *laser, bool trace) {
